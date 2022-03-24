98cbb69e30270fabd34fd27c234169d33fbf7e9d
ma-91-02 <ma.91.02@mail.ru>
2022-03-24 12:43:00 +0300

    update

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>oldstol</title>
+    <!-- Render All Elements Normally -->
+    <link rel="stylesheet" href="css/normalize.css" />
+    <!-- Font Awesome Library -->
+    <link rel="stylesheet" href="css/all.min.css" />
+    <!-- Main Template CSS File -->
+    <link rel="stylesheet" href="css/kasper.css" />
+    <link rel="stylesheet" href="css/your-ma.css">
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet" />
+  </head>
+  <body>
+     <!-- Start Header -->
+     <div class="header" id="header">
+      <div class="container">
+        <a href="#" class="logo">oldstol</a>
+        <ul class="main-nav">
+          <li><a href="#skidki">АКЦИИ И СКИДКИ</a></li>
+          <li><a href="#uslugi">Услуги</a></li>
+
+          <li><a href="#rassrochka">Рассрочка</a></li>
+          <li><a href="#materialy">Материалы</a></li>
+
+        </ul>
+      </div>
+    </div>
+    <!-- End Header -->
